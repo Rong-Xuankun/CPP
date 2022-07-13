06342ceaeff76db33566cb91f025176ba91bde1e
Xuankun Rong <971591116@qq.com>
2022-07-13 18:09:19 +0800

    Update

--- test.cpp
+++ test.cpp
@@ -1,78 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-int n, m, T, l, r;
-char maze[210][210];
-int vis[210][210][11];
-int dir[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
-int in(int x, int y)
-{
-    return 0 <= x && x < n && 0 <= y && y < m;
-}
+int n, max;
+int toy[10], likebest[10], pre[10];
+int like[10][10];
 
-struct node
+void dfs(int x, int ans)
 {
-    int x, y, d, c;
-} q[40010];
-int bfs(int sx, int sy)
-{
-    l = 0, r = 0;
-    struct node t = {sx, sy, 0, T};
-    q[r++] = t;
-    vis[sx][sy][t.c] = 1;
-    while (l < r)
+    if (x == n + 1)
     {
-        struct node now = q[l++];
-        if (maze[now.x][now.y] == '@')
+        if (ans > max)
+            max = ans;
+        return;
+    }
+    int i;
+    for (i = 1; i <= n; i++)
+    {
+        if (ans > max)
+            max = ans;
+        if (toy[i] == 0)
         {
-            return now.d;
-        }
-        for (int i = 0; i < 4; i++)
-        {
-            int tx = now.x + dir[i][0];
-            int ty = now.y + dir[i][1];
-            /*if (in(tx, ty) && maze[tx][ty] != '*' && !vis[tx][ty]) {
-                vis[tx][ty] = 1;
-                struct node t = {tx, ty, now.d + 1};
-                q[r++] = t;
-            }*/
-            if (in(tx, ty) && maze[tx][ty] == '#' && !vis[tx][ty][now.c] && now.c > 0)
+            if ((ans + pre[x]) <= max)
+                return;
+            else
             {
-                vis[tx][ty][now.c] = 1;
-                struct node t = {tx, ty, now.d + 1, now.c - 1};
-                q[r++] = t;
-            }
-            if (in(tx, ty) && maze[tx][ty] != '#' && !vis[tx][ty][now.c])
-            {
-                vis[tx][ty][now.c] = 1;
-                struct node t = {tx, ty, now.d + 1, now.c};
-                q[r++] = t;
+                toy[i] = 1;
+                dfs(x + 1, ans + like[x][i]);
+                toy[i] = 0;
             }
         }
     }
-    return -1;
 }
+
 int main()
 {
-    scanf("%d%d%d", &n, &m, &T);
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%s", maze[i]);
-    }
-    int x, y;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
+    int i, j, a, b;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
         {
-            if (maze[i][j] == '+')
-            {
-                x = i;
-                y = j;
-            }
+            scanf("%d", &a);
+            like[i][j] = a;
+        }
+    for (i = 1; i <= n; i++)
+    {
+        b = 0;
+        for (j = 1; j <= n; j++)
+        {
+            b = like[i][j] > b ? like[i][j] : b;
+        }
+        likebest[i] = b;
+    }
+    for (i = 1; i <= n; i++)
+    {
+        for (j = i; j <= n; j++)
+        {
+            pre[i] += likebest[j];
         }
     }
-    printf("%d\n", bfs(x, y));
+    dfs(1, 0);
+    printf("%d\n", max);
 
-    // system("PAUSE");
+    system("PAUSE");
     return 0;
 }
